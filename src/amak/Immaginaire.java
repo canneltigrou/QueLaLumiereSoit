b06cc90074808b9e46c246aa80966c45da2ad7dd
Claire <canneltigrou@gmail.com>
2018-07-21 02:49:41 +0200

    javaDoc Migrant + imaginaire

--- src/amak/Immaginaire.java
+++ src/amak/Immaginaire.java
@@ -6,133 +6,151 @@ import business.Blob;
 import business.Critere;
 import fr.irit.smac.amak.tools.Log;
 
-public class Immaginaire extends BlobAgent{
+/**
+ * Classe fille de Blob Agent. Agent lié à un blob virtuel, présent uniquement
+ * dans Tidel.
+ * <p>
+ * Il peut décider de Créer ; se suicider ; se déplacer ; changer de formes.
+ * </p>
+ * 
+ * @author Claire MEVOLHON
+ *
+ */
+public class Immaginaire extends BlobAgent {
 
 	public Immaginaire(MyAMAS amas, Blob b, Controller controller) {
 		super(amas, b, controller);
-		
+
 	}
-	
+
 	@Override
-    protected double computeCriticality() {
-		return(computeCriticalityInTideal());
+	protected double computeCriticality() {
+		return (computeCriticalityInTideal());
 	}
-	
+
+	/**
+	 * Fonction de mise à jour de l'affichage appelée à chaque cycle de AMAK : on ne
+	 * décide d'afficher qu'à une certaine fréquence. (sauf pour les actions
+	 * importantes : création et suicide)
+	 */
 	@Override
-    protected void onUpdateRender() {
-		if (tps + 300 > System.currentTimeMillis() && !(currentAction.equals(Action.CREER) || currentAction.equals(Action.SE_SUICIDER))) {
+	protected void onUpdateRender() {
+		if (tps + 300 > System.currentTimeMillis()
+				&& !(currentAction.equals(Action.CREER) || currentAction.equals(Action.SE_SUICIDER))) {
 			return;
 		}
 		tps = System.currentTimeMillis();
 		try {
-			switch(currentAction){
-	    	case SE_DEPLACER :
-	    		synchronized (this)
-	            {
-	        		controller.move_blobImmaginaire(this);
-	            }
-	    		break;
-	    	case CREER :
-	    		synchronized (this)
-	            {
-	    			controller.add_blobImmaginaire(newFils);
-	            }
-	    		break;
-	    	case SE_SUICIDER :
-	    		synchronized (this)
-	            {
-	    			controller.remove_blobImmaginaire(this);
-	            }
+			switch (currentAction) {
+			case SE_DEPLACER:
+				synchronized (this) {
+					controller.move_blobImmaginaire(this);
+				}
+				break;
+			case CREER:
+				synchronized (this) {
+					controller.add_blobImmaginaire(newFils);
+				}
+				break;
+			case SE_SUICIDER:
+				synchronized (this) {
+					controller.remove_blobImmaginaire(this);
+				}
 				break;
 			default:
 				break;
-	    	}
-    	} catch(Exception e)
-		{
+			}
+		} catch (Exception e) {
 			ExceptionHandler eh = new ExceptionHandler();
 			eh.showError(e);
 		}
 
-    }
-	
+	}
+
+	/** @see amak.BlobAgent#action_se_deplacer(); */
 	@Override
-	protected void action_se_deplacer(){
+	protected void action_se_deplacer() {
 		double[] tmp = getAmas().getEnvironment().nouvellesCoordonnees(this, Math.random() * 0.7, pastDirection);
 		blob.setCoordonnee(tmp);
 		currentAction = Action.SE_DEPLACER;
-		
+
 		directGeneral[0] = 0.6 * pastDirection[0] + 0.4 * directGeneral[0];
 		directGeneral[1] = 0.6 * pastDirection[1] + 0.4 * directGeneral[1];
 	}
-	
-	
+
 	@Override
 	protected void onDecideAndAct() {
 		try {
-			synchronized(blob.lock){ 
-				 majAspectAgent();		 
-				 currentAction = Action.RESTER; // to initialise
-			     BlobAgent agentNeedingHelp = super.getMoreCriticalAgent();
-				 Critere most_critic = Most_critical_critere(agentNeedingHelp.getCriticite());
-				 //System.out.println("criticite de stabilitï¿½ de position = " + criticite[Critere.Stabilite_position.getValue()]);
-				 //System.out.println("le plus critique est : " + most_critic.toString());
-				 
-				 switch (most_critic){
-				 case Isolement:
-					 // too many neighboors -> criticite.ISOLEMENT<0 -> I have to kill myself
-					 if(criticite[Critere.Isolement.getValue()] < 0)
-						 action_se_suicider();
-					 else
-						 action_creer();
-					 break;
-						 
-				 case Stabilite_etat:
+			synchronized (blob.lock) {
+				majAspectAgent();
+				currentAction = Action.RESTER; // to initialise
+				BlobAgent agentNeedingHelp = super.getMoreCriticalAgent();
+				Critere most_critic = Most_critical_critere(agentNeedingHelp.getCriticite());
 
-						Log.debug("quela", "imag decide stabeta");
-					 
-					 
-					 break;
-					 
-				 case Stabilite_position:
-						Log.debug("quela", "imag decide stab pos");
-					 action_se_deplacer();
-					 break;
-					 
-				 case Heterogeneite:
-						Log.debug("quela", "imag decide eheteroge");
-					 //System.out.println(" \t avec : " + criticite[Critere.Heterogeneite.getValue()]);
+				switch (most_critic) {
+				case Isolement:
+					// too many neighboors -> criticite.ISOLEMENT<0 -> I have to kill myself
+					if (criticite[Critere.Isolement.getValue()] < 0)
+						action_se_suicider();
+					else
+						action_creer();
+					break;
+
+				case Stabilite_etat:
+
+					Log.debug("quela", "imag decide stabeta");
+
+					break;
+
+				case Stabilite_position:
+					Log.debug("quela", "imag decide stab pos");
+					action_se_deplacer();
+					break;
+
+				case Heterogeneite:
+					Log.debug("quela", "imag decide eheteroge");
+					// System.out.println(" \t avec : " +
+					// criticite[Critere.Heterogeneite.getValue()]);
 					// if >0 then it's too homogeneous. --> I change the color in a random one.
-					 // else it's too heterogeneous.  -> I change my color to the most present color
-					 if(criticite[Critere.Heterogeneite.getValue()] > 0)
-						 action_changerCouleur();
-					 else
-						 action_changerCouleur(couleurEnvironnante);
-					 break;
-				 
-				 default:
-					break;		 
-				 }
+					// else it's too heterogeneous. -> I change my color to the most present color
+					if (criticite[Critere.Heterogeneite.getValue()] > 0)
+						action_changerCouleur();
+					else
+						action_changerCouleur(couleurEnvironnante);
+					break;
+
+				default:
+					break;
+				}
 			}
 
 			Log.debug("quela", "imag decide before super decide and act");
 			super.onDecideAndAct();
 
 			Log.debug("quela", "imag decide before catch");
-		} catch(Exception e)
-		{
+		} catch (Exception e) {
 			Log.debug("quela", "imag decide catch");
 			ExceptionHandler eh = new ExceptionHandler();
 			eh.showError(e);
 		}
 		Log.debug("quela", "end da immag");
 	}
-	
-protected void onAgentCycleEnd() {
-		
-		if(currentAction.equals(Action.CREER))
+
+	/**
+	 * Actions exécutés en fin de cycle de Amak :
+	 * <ul>
+	 * <li>On met à jour la liste des agents de l'environnement en cas de création
+	 * d'un blob.<br>
+	 * La raison est que la création ou la suppression d'un agent n'est effectif
+	 * qu'au cycle suivant. On ne veut donc pas que l'environnement utilise ce
+	 * nouvel agent avant le cycle suivant.</li>
+	 * </ul>
+	 */
+	protected void onAgentCycleEnd() {
+
+		if (currentAction.equals(Action.CREER))
 			getAmas().getEnvironment().addAgent(newFils);
 		super.onAgentCycleEnd();
 	}
-	
 
 }
--- src/amak/Migrant.java
+++ src/amak/Migrant.java
@@ -5,20 +5,36 @@ import application.ExceptionHandler;
 import business.Blob;
 import business.Critere;
 
-public class Migrant extends BlobAgent{
-	
-	
+/**
+ * Classe fille de {@link amak.BlobAgent BlobAgent}. <br>
+ * Agent lié à un blob "réel" : soit dans To, soit dans Tr.
+ * <p>
+ * Ses actions dépendent s'il se situe dans To ou Tr :<br>
+ * <ul>
+ * <li>Dans To : Il peut mûrir ou se déplacer.</li>
+ * <li>Dans Tr (lié à un spectateur): Il peut créer ou changer de forme. Il ne
+ * peut ni se suicider, ni se déplacer.
+ * </ul>
+ * 
+ * 
+ * @author Claire MEVOLHON
+ *
+ */
+public class Migrant extends BlobAgent {
+
+	/** true si l'agent est dans To. False si dans Treel */
 	private boolean isHome;
+	/** true si l'agent est mûr, ie prêt à partir de To */
 	private boolean isRiped;
+	/** compteur : nombre de cycles depuis lequel l'agent est mûr. */
 	private int cptRiped;
-	//private int cpt_hibernation;
-	//private int nbRipedIdeal = 1;
+	/** taux de mûrissement dans To (en pourcentage) */
 	private double tauxMurissement = 5;
+	/** nouvelles coordonnées du blob qui seront à affecter en fin de cycle */
 	private double[] cooFutur;
+	/** true si de nouvelles coordonnées sont à affecter en fin de cycle */
 	private boolean isGoingToMove;
-	
-	
-	
+
 	public Migrant(MyAMAS amas, Blob b, Controller controller) {
 		super(amas, b, controller);
 		isHome = true;
@@ -34,182 +50,177 @@ public class Migrant extends BlobAgent{
 	public void setHome(boolean isHome) {
 		this.isHome = isHome;
 	}
-	
-	
-	// boolean renvoyant true avec une probabilité de 'tauxMurissement' géré dans l'IHM.
-	private boolean mustRipe(){
-		return( Math.random() * 100 < tauxMurissement);
+
+	/** boolean renvoyant true avec une probabilité de 'tauxMurissement' */
+	private boolean mustRipe() {
+		return (Math.random() * 100 < tauxMurissement);
 	}
-	
-	
-	/*
-	private double computeCriticalityMurissement(){
-		// je compte le nombre de voisins murs autour de moi.
-		double cpt = 0;
-		for (int i = 0; i< voisins.size(); i++){
-			if( ( (Migrant)(voisins.get(i))).isRiped)
-				cpt++;
-		}
-		return(nbRipedIdeal - cpt);
-	}
-	
-	private double computeCriticalityPositionTo(){
-		// je compte le nombre de voisins qui bougent autour de moi.
-		double cpt = 0;
-		for (int i = 0; i< voisins.size(); i++){
-			if( ( (Migrant)(voisins.get(i))).isRiped)
-				cpt++;
-		}
-		return(nbRipedIdeal - cpt);
-	}
-	
-	private double computeCriticalityIsolementTo(){
-		//if (nbBlobs / 2 > 1 )
-		
-		return(getAmas().getEnvironment().getIsolement() - voisins.size());
-	}
-	*/
-	
-	
-	private void action_murir(){
+
+	/** action de murir */
+	private void action_murir() {
 		isRiped = true;
 		cptRiped = 20;
 	}
-	
+
 	@Override
 	protected void onDecideAndAct() {
 		try {
-			synchronized(blob.lock)
-			{
+			synchronized (blob.lock) {
 				currentAction = Action.RESTER; // to initialise
-				if (isHome){
-					if(isRiped){
-						if(cptRiped <= 0)
+				if (isHome) {
+					if (isRiped) {
+						if (cptRiped <= 0)
 							isRiped = false;
 						else
 							cptRiped--;
-					}
-					else
-						if(mustRipe())
-							action_murir(); //isRiped = true;
-					
+					} else if (mustRipe())
+						action_murir(); // isRiped = true;
+
 					action_se_deplacer();
-				}			
-				else
+				} else
 					majAspectAgent();
 				BlobAgent agentNeedingHelp = super.getMoreCriticalAgent();
 				Critere most_critic = Most_critical_critere(agentNeedingHelp.getCriticite());
-				
-				// Si je suis sans TR/TI ne peux pas me mouvoir. Je ne peux donc pas gérer la criticité de position
+
+				// Si je suis sans TR/TI ne peux pas me mouvoir. Je ne peux donc pas gérer la
+				// criticité de position
 				// Je vais aider le plus critique sur une autre de ses criticités.
-				if (!isHome && most_critic == Critere.Stabilite_position){
+				if (!isHome && most_critic == Critere.Stabilite_position) {
 					double[] tmp = agentNeedingHelp.getCriticite();
 					tmp[Critere.Heterogeneite.getValue()] = 0;
 					most_critic = Most_critical_critere(tmp);
 				}
-				
-				 switch (most_critic){
-				 case Isolement:
-					 // too few neighboors -> criticite.ISOLEMENT > 0 -> I have procreate
-					 if(criticite[Critere.Isolement.getValue()] > 0)
-						 action_creer();
-					 break;
-						 
-				 case Stabilite_position:					 
-					 break;
-					 
-				 case Heterogeneite:
-					 // if >0 then it's too homogeneous. --> I change the color in a random one.
-					 // else it's too heterogeneous.  -> I change my color to the most present color
-					 if(criticite[Critere.Heterogeneite.getValue()] > 0)
-						 action_changerCouleur();
-					 else
-						 action_changerCouleur(couleurEnvironnante);
-		
-					 break;
-				 
-				 default:
-					break;		 
-				 }
+
+				switch (most_critic) {
+				case Isolement:
+					// too few neighboors -> criticite.ISOLEMENT > 0 -> I have procreate
+					if (criticite[Critere.Isolement.getValue()] > 0)
+						action_creer();
+					break;
+
+				case Stabilite_position:
+					break;
+
+				case Heterogeneite:
+					// if >0 then it's too homogeneous. --> I change the color in a random one.
+					// else it's too heterogeneous. -> I change my color to the most present color
+					if (criticite[Critere.Heterogeneite.getValue()] > 0)
+						action_changerCouleur();
+					else
+						action_changerCouleur(couleurEnvironnante);
+
+					break;
+
+				default:
+					break;
+				}
 			}
 			super.onDecideAndAct();
-		} catch(Exception e)
-		{
-			ExceptionHandler eh = new ExceptionHandler();
-			eh.showError(e);
-		}	
-	}
-	
-	
-	@Override
-    protected void onUpdateRender() {
-		
-		if (tps + 300 > System.currentTimeMillis() && !(currentAction.equals(Action.CREER) || currentAction.equals(Action.MURIR))) {
-			return;
-		}
-		tps = System.currentTimeMillis();
-		try {
-	    	switch(currentAction){
-	    	case CREER :
-	    		synchronized (this)
-	            {
-	    			super.controller.add_blobImmaginaire(newFils);
-	            }
-				break;
-	    		
-			default:
-				if(isHome)
-					synchronized (this)
-			        {
-						super.controller.move_blobHibernant(this);
-			        }
-	    		else
-	    			synchronized (this)
-	    	        {
-	    				super.controller.move_blobMigrant(this);
-	    	        }
-	    		break;
-	    	}
-		} catch(Exception e)
-		{
-			ExceptionHandler eh = new ExceptionHandler();
-			eh.showError(e);
-		}
-    }
-	
-	public void t0_to_tr(){
-		try {
-			isHome = false;
-			blob.setCoordonnee(blob.genererCoordonneeAleaDansCercle(getAmas().getEnvironment().rayonTerrain * 2));
-			getAmas().getEnvironment().t0_to_tr(this);
-			controller.add_blobMigrant(this);
-			controller.remove_blobHibernant(this);
-		} catch(Exception e)
-		{
-			ExceptionHandler eh = new ExceptionHandler();
-			eh.showError(e);
-		}
-	}
-	
-	public void t0_to_tr(double[] coo){
-		try {
-			isHome = false;
-			blob.setCoordonnee(coo);
-			getAmas().getEnvironment().t0_to_tr(this);
-			controller.add_blobMigrant(this);
-			controller.remove_blobHibernant(this);
-		} catch(Exception e)
-		{
+		} catch (Exception e) {
 			ExceptionHandler eh = new ExceptionHandler();
 			eh.showError(e);
 		}
 	}
 
 	@Override
+	protected void onUpdateRender() {
+
+		if (tps + 300 > System.currentTimeMillis()
+				&& !(currentAction.equals(Action.CREER) || currentAction.equals(Action.MURIR))) {
+			return;
+		}
+		tps = System.currentTimeMillis();
+		try {
+			switch (currentAction) {
+			case CREER:
+				synchronized (this) {
+					super.controller.add_blobImmaginaire(newFils);
+				}
+				break;
+
+			default:
+				if (isHome)
+					synchronized (this) {
+						super.controller.move_blobHibernant(this);
+					}
+				else
+					synchronized (this) {
+						super.controller.move_blobMigrant(this);
+					}
+				break;
+			}
+		} catch (Exception e) {
+			ExceptionHandler eh = new ExceptionHandler();
+			eh.showError(e);
+		}
+	}
+
+	/**
+	 * Passage de l'agent de To à Tr. Ici l'agent prendra une coordonnée aléatoire
+	 * possible dans Tr.
+	 * <p>
+	 * ATTENTION : À changer plus tard si procréation possible dans To. <br>
+	 * (Ici, comme il n'y a pas de création de blob dans To, il n'y a pas de risque
+	 * lié au changement de coordonnées. Tout est fait immédiatement.)
+	 * </p>
+	 */
+	public void t0_to_tr() {
+		try {
+			isHome = false;
+			blob.setCoordonnee(blob.genererCoordonneeAleaDansCercle(getAmas().getEnvironment().rayonTerrain * 2));
+			getAmas().getEnvironment().t0_to_tr(this);
+			controller.add_blobMigrant(this);
+			controller.remove_blobHibernant(this);
+		} catch (Exception e) {
+			ExceptionHandler eh = new ExceptionHandler();
+			eh.showError(e);
+		}
+	}
+
+	/**
+	 * Passage de l'agent de To à Tr. Ici l'agent se positionnera à la coordonnée
+	 * donnée en paramètre.
+	 * <p>
+	 * ATTENTION : À changer plus tard si procréation possible dans To. <br>
+	 * (Ici, comme il n'y a pas de création de blob dans To, il n'y a pas de risque
+	 * lié au changement de coordonnées. Tout est fait immédiatement.)
+	 * </p>
+	 * 
+	 * @param coo
+	 *            coordonnées à prendre dans Tr.
+	 */
+	public void t0_to_tr(double[] coo) {
+		try {
+			isHome = false;
+			blob.setCoordonnee(coo);
+			getAmas().getEnvironment().t0_to_tr(this);
+			controller.add_blobMigrant(this);
+			controller.remove_blobHibernant(this);
+		} catch (Exception e) {
+			ExceptionHandler eh = new ExceptionHandler();
+			eh.showError(e);
+		}
+	}
+
+	/**
+	 * Actions exécutés en fin de cycle de Amak :
+	 * <ul>
+	 * <li>On met à jour la liste des agents de l'environnement en cas de création
+	 * d'un blob.<br>
+	 * La raison est que la création ou la suppression d'un agent n'est effectif
+	 * qu'au cycle suivant. On ne veut donc pas que l'environnement utilise ce
+	 * nouvel agent avant le cycle suivant.</li>
+	 *
+	 * <li>"isGoingToMove" quand un agent passe de Tr à To : on met à jour ses
+	 * nouvelles coordonnées et l'environnement. (Ainsi, si notre agent était au
+	 * milieu de son cycle, ses actions s'effectueront avec les attributs qu'il
+	 * avait lors de sa décision.)</li>
+	 * </ul>
+	 */
+	@Override
 	protected void onAgentCycleEnd() {
-		
-		if(isGoingToMove)
-		{
+
+		if (isGoingToMove) {
 			isHome = true;
 			isGoingToMove = false;
 			getAmas().getEnvironment().tr_to_t0(this);
@@ -217,67 +228,59 @@ public class Migrant extends BlobAgent{
 			controller.add_blobHibernant(this);
 			controller.remove_blobMigrant(this);
 		}
-		if(currentAction.equals(Action.CREER))
+		if (currentAction.equals(Action.CREER))
 			getAmas().getEnvironment().addAgent(newFils);
-		
-		
+
 		super.onAgentCycleEnd();
 	}
-	
-	public void tr_to_t0(){
+
+	/**
+	 * Passage de l'agent de Tr à To. L'agent prendra une coordonnée aléatoire
+	 * possible dans To.
+	 * <p>
+	 * Cette méthode est appelée depuis un thread exterieur à AMAS. Elle peut donc
+	 * s'exécuter en plein cycle de l'agent. Pour éviter certains problèmes (comme
+	 * créer un fils avec des coordonnées hors-map) les changements de coordonées
+	 * s'effectueront la fin du cycle dans la methode {@link #onAgentCycleEnd()
+	 * onAgentCycleEnd}
+	 * </p>
+	 */
+	public void tr_to_t0() {
 		try {
-			
+
 			cooFutur = blob.genererCoordonneeAleaDansCercle(100);
 			isGoingToMove = true;
 			// j'attends la fin du cycle pour me mettre à jour
-			//blob.setCoordonnee(blob.genererCoordonneeAleaDansCercle(100));
+			// blob.setCoordonnee(blob.genererCoordonneeAleaDansCercle(100));
 			/*
-			getAmas().getEnvironment().tr_to_t0(this);
-			controller.add_blobHibernant(this);
-			controller.remove_blobMigrant(this);*/
-		}catch(Exception e)
-		{
+			 * getAmas().getEnvironment().tr_to_t0(this);
+			 * controller.add_blobHibernant(this); controller.remove_blobMigrant(this);
+			 */
+		} catch (Exception e) {
 			ExceptionHandler eh = new ExceptionHandler();
 			eh.showError(e);
 		}
 	}
-	
-//	public void tr_to_t0(double[] coo){
-//		try {
-//			isHome = true;
-//			blob.setCoordonnee(coo);
-//			getAmas().getEnvironment().tr_to_t0(this);
-//			controller.add_blobHibernant(this);
-//			controller.remove_blobMigrant(this);
-//		} catch(Exception e)
-//		{
-//			ExceptionHandler eh = new ExceptionHandler();
-//			eh.showError(e);
-//		}
-//	}
 
-	
-	
-	/*private double computeCriticalityInTo(){
-		criticite[Critere.Murissement.getValue()] =	computeCriticalityMurissement();
-		criticite[Critere.Isolement.getValue()] = computeCriticalityIsolementTo();
-		criticite[Critere.Stabilite_position.getValue()] = computeCriticalityPositionTo();
-
-		return criticite[Critere.Murissement.getValue()]; // TODO
-	}*/
-	
+	/**
+	 * Calcule de la criticité. Laquelle dépend si l'agent se situe actuellement en
+	 * To ou Tr. <br>
+	 * La criticité dans Tr se calcule de la même façon qu'un agent immaginaire de
+	 * Ti (seules les décisions diffèrent). <br>
+	 * Pour le moment, il n'y a pas d'action coopératives dans To. Donc pas de
+	 * calcul de criticité.
+	 */
 	@Override
-    protected double computeCriticality() {
+	protected double computeCriticality() {
 		double res = 0;
 		try {
-		if(!isHome)
-			res = computeCriticalityInTideal();
-		} catch(Exception e)
-		{
+			if (!isHome)
+				res = computeCriticalityInTideal();
+		} catch (Exception e) {
 			ExceptionHandler eh = new ExceptionHandler();
 			eh.showError(e);
 		}
-		return(res);
+		return (res);
 	}
 
 	public boolean isRiped() {
@@ -287,8 +290,5 @@ public class Migrant extends BlobAgent{
 	public void setRiped(boolean isRiped) {
 		this.isRiped = isRiped;
 	}
-	
-	
-	
-	
+
 }
